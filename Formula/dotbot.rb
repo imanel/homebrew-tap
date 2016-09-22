58a5db9758c350a4353877984e23047f52746f80
Bernard Potocki <bernard.potocki@imanel.org>
2016-09-22 09:17:31 +0200

    Use sha256 as recommended

--- Formula/dotbot.rb
+++ Formula/dotbot.rb
@@ -2,7 +2,7 @@ class Dotbot < Formula
   desc "A tool that bootstraps your dotfiles."
   homepage "https://github.com/anishathalye/dotbot"
   url "https://github.com/anishathalye/dotbot/archive/v1.9.3.tar.gz"
-  sha1 "2a967d5a33c223d256ba16173681b41763a7014d"
+  sha256 "bd87e579c4db29eaca7a25a76de7279f03977b8e2778ef8b9ec0996c854e7b18"
   head "https://github.com/anishathalye/dotbot.git"
 
   def install
